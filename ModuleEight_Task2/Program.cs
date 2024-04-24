bdf583a643bb5981af49f0c96fdf77a4756236ed
Полина Артамонова <poliiiiinka71@gmail.com>
2024-04-24 18:12:30 +0300

    Добавьте файлы проекта.

---
+++ ModuleEight_Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ModuleEight_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите адрес папки: ");
+            string FolderPath = Console.ReadLine();
+            Console.WriteLine(FolderSize(FolderPath));
+        }
+
+        public static long FolderSize(string path)
+        {
+            DirectoryInfo directory;
+            if (Directory.Exists(path))
+            {
+                directory = new DirectoryInfo(path);
+                
+                long size = 0;
+
+                try
+                {
+                    foreach (var file in directory.GetFiles())
+                        size += file.Length;
+                }
+                catch { }
+
+                try
+                {
+                    foreach (var direct in directory.GetDirectories())
+                        size += FolderSize(direct.FullName);
+                }
+                catch { }
+
+                return size;
+            }
+            else
+            {
+                Console.WriteLine("Папка не найдена!");
+                return 0;
+            }
+        }
+    }
+}
